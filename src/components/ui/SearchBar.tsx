6faad87c9fd95cd77ed2b58a485a25a672f02833
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2025-01-18 15:30:24 +0600

    respnsive home

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -17,28 +17,29 @@ export default function SearchBar() {
         <span className="">
             {
                 filter ? (
-                    <div className="max-w-3xl mx-auto">
-
-                        <div className="relative flex   gap-2 px-4">
+                    <div className="max-w-3xl mx-auto px-2">
+                        <div className="relative flex flex-col sm:flex-row sm:gap-2 px-4 sm:px-0">
                             <div className="relative flex-1">
                                 <IoMdSearch className="absolute top-2 left-1 z-40 text-white w-9 h-9" />
-                                <TbMenuDeep onClick={() => setFilter(false)} className="absolute top-2 right-1 z-40 text-white w-9 h-9 cursor-pointer hover:text-slate-300" />
+                                <TbMenuDeep
+                                    onClick={() => setFilter(false)}
+                                    className="absolute top-2 right-1 z-40 text-white w-9 h-9 cursor-pointer hover:text-slate-300"
+                                />
                                 <input
                                     type="text"
                                     style={{ paddingLeft: '40px' }}
                                     placeholder="GMT-Master for 38mm"
-                                    className="h-12 w-full rounded-[8px] max-w-[858px] font-medium bg-[#ECECEC59] pl-4 text-white  placeholder:text-white/70 "
+                                    className="h-12 w-full rounded-[8px] max-w-[858px] font-medium bg-[#ECECEC59] pl-4 text-white placeholder:text-white/70"
                                 />
                             </div>
                             <Button
                                 onClick={handleSearch}
-                                className="h-12 px-9  text-[20px] font-medium bg-slate-800 hover:bg-slate-700"
+                                className="h-12 sm:px-9 px-4 mt-2 sm:mt-0 text-[20px] font-medium bg-slate-800 hover:bg-slate-700"
                             >
                                 Search
                             </Button>
                         </div>
                     </div>
-
                 ) : (
                     <DropdownMenu />
                 )
--- src/components/ui/WatchFilters.tsx
+++ src/components/ui/WatchFilters.tsx
@@ -3,13 +3,16 @@
 import * as React from "react"
 import { IoMdSearch } from "react-icons/io"
 import { Button } from "@/components/ui/button"
-import { ChevronDown } from 'lucide-react'
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { useRouter } from "next/navigation"
+import { MdArrowDropDown } from "react-icons/md";
+
 
 const brands = [
     "Rolex", "Omega", "Tag Heuer", "Breitling", "Casio", "Citizen", "Seiko",
@@ -22,107 +25,130 @@ const models = [
 ]
 const sizes = ["34mm", "36mm", "38mm", "40mm", "42mm", "44mm", "46mm", "48mm"]
 
+interface WatchFiltersForm {
+    brand: string;
+    model: string;
+    size: string;
+}
+
 export default function WatchFilters() {
+    const { handleSubmit, setValue } = useForm<WatchFiltersForm>(); // Specify the type here
     const [selectedBrand, setSelectedBrand] = React.useState("")
     const [selectedModel, setSelectedModel] = React.useState("")
     const [selectedSize, setSelectedSize] = React.useState("")
 
+
+    const router = useRouter()
+
+    const onSubmit: SubmitHandler<WatchFiltersForm> = (data) => {
+        console.log(data);
+        router.push('/search-result')
+    }
+
     return (
         <div className="w-full max-w-[1100px] mx-auto">
-            <div className="grid bg-white py-4 px-6 rounded-[8px]  p-4 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 items-center gap-3"
-            >
-
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <div className="w-full md:w-[280px] cursor-pointer mr-4">
-                            <div className="flex items-center justify-between w-full p-3 border rounded-md bg-white hover:border-gray-400">
-                                <div>
-                                    <div className="text-[20px] font-semibold mb-1">{selectedBrand || "Brand"}</div>
-                                    <div className="text-gray-500">
-                                        {selectedBrand ? "": "Select Brand"}
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid bg-white py-4 px-6 rounded-[8px] p-4 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 items-center gap-3">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <div className="w-full md:w-[280px] cursor-pointer mr-4 ">
+                                <div className="flex items-center gap-2 w-full p-3  rounded-md bg-white hover:border-gray-400">
+                                    <div>
+                                        <div className="text-[20px] font-semibold mb-1 flex gap-3">{selectedBrand || "Brand"}                                     <MdArrowDropDown className="h-7 w-7 text-black" /></div>
+                                        <div className="text-gray-500">
+                                            {selectedBrand ? "" : "Select Brand"}
+                                        </div>
                                     </div>
                                 </div>
-                                <ChevronDown className="h-5 w-5 text-gray-500" />
                             </div>
-                        </div>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-[280px] max-h-[280px] overflow-y-auto">
-                        {brands.map((brand) => (
-                            <DropdownMenuItem
-                                key={brand}
-                                className="py-2 cursor-pointer"
-                                onClick={() => setSelectedBrand(brand)}
-                            >
-                                {brand}
-                            </DropdownMenuItem>
-                        ))}
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-[280px] max-h-[280px] overflow-y-auto">
+                            {brands.map((brand) => (
+                                <DropdownMenuItem
+                                    key={brand}
+                                    className="py-2 cursor-pointer"
+                                    onClick={() => {
+                                        setSelectedBrand(brand);
+                                        setValue("brand", brand); // Setting value in react-hook-form
+                                    }}
+                                >
+                                    {brand}
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
 
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <div className="w-full md:w-[280px] cursor-pointer">
-                            <div className="flex items-center justify-between w-full p-3 border rounded-md bg-white hover:border-gray-400">
-                                <div>
-                                    <div className="text-[20px] font-semibold mb-1">{selectedModel || "Model"}</div>
-                                    <div className="text-gray-500">
-                                        {selectedModel? "": "Select Model"}
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <div className="w-full md:w-[280px] cursor-pointer">
+                                <div className="flex items-center gap-2 w-full p-3  rounded-md bg-white hover:border-gray-400">
+                                    <div>
+                                        <div className="text-[20px] font-semibold mb-1 flex gap-3">{selectedModel || "Model"} <MdArrowDropDown className="h-7 w-7 text-black" /></div>
+                                        <div className="text-gray-500">
+                                            {selectedModel ? "" : "Select Model"}
+                                        </div>
                                     </div>
+                                    
                                 </div>
-                                <ChevronDown className="h-5 w-5 text-gray-500" />
                             </div>
-                        </div>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-[280px] max-h-[280px] overflow-y-auto">
-                        {models.map((model) => (
-                            <DropdownMenuItem
-                                key={model}
-                                className="py-2 cursor-pointer"
-                                onClick={() => setSelectedModel(model)}
-                            >
-                                {model}
-                            </DropdownMenuItem>
-                        ))}
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-[280px] max-h-[280px] overflow-y-auto">
+                            {models.map((model) => (
+                                <DropdownMenuItem
+                                    key={model}
+                                    className="py-2 cursor-pointer"
+                                    onClick={() => {
+                                        setSelectedModel(model);
+                                        setValue("model", model); // Setting value in react-hook-form
+                                    }}
+                                >
+                                    {model}
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
 
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <div className="w-full md:w-[280px] cursor-pointer">
-                            <div className="flex items-center justify-between w-full p-3 border rounded-md bg-white hover:border-gray-400">
-                                <div>
-                                    <div className="text-[20px] font-semibold mb-1"> {selectedSize || "Size"}</div>
-                                    <div className="text-gray-500">
-                                        {selectedSize ? "": "Select Size"}
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <div className="w-full md:w-[280px] cursor-pointer">
+                                <div className="flex items-center gap-2 w-full p-3  rounded-md bg-white hover:border-gray-400">
+                                    <div>
+                                        <div className="text-[20px] font-semibold mb-1 flex gap-3">{selectedSize || "Size"} <MdArrowDropDown className="h-7 w-7 text-black" /></div>
+                                        <div className="text-gray-500">
+                                            {selectedSize ? "" : "Select Size"}
+                                        </div>
                                     </div>
+                                    
                                 </div>
-                                <ChevronDown className="h-5 w-5 text-gray-500" />
                             </div>
-                        </div>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-[280px] max-h-[280px] overflow-y-auto">
-                        {sizes.map((size) => (
-                            <DropdownMenuItem
-                                key={size}
-                                className="py-2 cursor-pointer"
-                                onClick={() => setSelectedSize(size)}
-                            >
-                                {size}
-                            </DropdownMenuItem>
-                        ))}
-                    </DropdownMenuContent>
-                </DropdownMenu>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-[280px] max-h-[280px] overflow-y-auto">
+                            {sizes.map((size) => (
+                                <DropdownMenuItem
+                                    key={size}
+                                    className="py-2 cursor-pointer"
+                                    onClick={() => {
+                                        setSelectedSize(size);
+                                        setValue("size", size); // Setting value in react-hook-form
+                                    }}
+                                >
+                                    {size}
+                                </DropdownMenuItem>
+                            ))}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
 
-                <div className="flex justify-end">
-                    <Button
-                        className="w-full md:w-[160px] h-[72px] bg-[#2C3641] hover:bg-[#3A4754] text-white rounded-md flex items-center justify-center gap-2"
-                    >
-                        <IoMdSearch className="w-5 h-5" />
-                        Search
-                    </Button>
+                    <div className="flex lg:justify-end">
+                        <Button
+                            type="submit"
+                            className="w-full md:w-[160px] h-[72px] bg-[#2C3641] hover:bg-[#3A4754] text-white rounded-md flex items-center justify-center gap-2"
+                        >
+                            <IoMdSearch className="w-5 h-5" />
+                            Search
+                        </Button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
-
